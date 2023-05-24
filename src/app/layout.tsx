c375a57ef8205f89b0cf548229a450a32505ebee
Luis Medina <lamc3005@gmail.com>
2023-05-23 22:04:21 -0400

    cambiar el title, favicon y la description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
+import { Metadata } from 'next'
 import './globals.css'
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export const metadata = {
+export const metadata: Metadata = {
 	title: 'Pokedex',
 	description: 'App created for learn to use the new version of NextJS',
 }
@@ -15,7 +13,7 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<body className={inter.className}>{children}</body>
+			<body>{children}</body>
 		</html>
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
-export default function Home() {
+import Navbar from '@/features/navbar/navbar'
+import { PokemonList } from '@/features/pokemons'
+
+function Home() {
 	return (
-		<main className=''>
-			<div></div>
+		<main className='bg-identity-primary w-full p-1 '>
+			<Navbar />
+			<PokemonList />
 		</main>
 	)
 }
+export default Home
