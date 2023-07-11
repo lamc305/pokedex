f282b2c231cca05a1c07ecc8fa0063b5ec77b501
Luis Medina <lamc3005@gmail.com>
2023-07-11 19:23:04 -0400

    feat(dependencies): instalation of some dependecies like react hook form, jest, prettier....

--- src/features/pokemons/index.tsx
+++
@@ -1,9 +0,0 @@
-import Pokemons, { Params } from './pokemon'
-
-export function PokemonList({ params }: Params) {
-	return (
-		<div className='bg-grayscale-white min-h-[575px] max-w-2xl mx-auto shadow-inner-2-dp rounded-lg pt-6 px-3 pb-6'>
-			<Pokemons params={params} />
-		</div>
-	)
-}
